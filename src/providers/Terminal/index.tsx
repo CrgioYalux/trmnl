c605bf3227977b35a7d116bac75e212967a5d8dd
Sergio Yalux <randaledomman1@gmail.com>
2023-02-26 03:58:08 -0300

    ref: terminal context provider uses useTerminal hook instead of implementing the logic itself

--- src/providers/Terminal/index.tsx
+++ src/providers/Terminal/index.tsx
@@ -1,15 +1,28 @@
-import { useTerminalContext } from './useTerminalContext';
-import { Context } from "./useTerminalContext";
+import { createContext, useContext } from 'react';
+import useTerminal from '../../hooks/useTerminal';
+import type { useTerminalState } from '../../hooks/useTerminal';
 
 interface TerminalProviderProps {
     children: React.ReactNode;
 }
 
-export const TerminalProvider: React.FC<TerminalProviderProps> = ({ children }) => {
-    const value = useTerminalContext();
+type TerminalContext = readonly [
+    state: useTerminalState[0],
+    actions: useTerminalState[1],
+];
+
+const Context = createContext<TerminalContext>([{ directoryTree: [], logs: [], prompt: '' }, { interpretInput: () => {} }]);
+
+const useTerminalContext = () => useContext<TerminalContext>(Context);
+
+const TerminalProvider: React.FC<TerminalProviderProps> = ({ children }) => {
+    const value = useTerminal();
+
     return (
         <Context.Provider value={value}>
-            {children}
+        {children}
         </Context.Provider>
     );
 };
+
+export { TerminalProvider, useTerminalContext };
--- src/providers/Terminal/useTerminal/index.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import {  Context } from "../useTerminalContext";
-import type { TerminalContext } from "../useTerminalContext";
-
-export const useTerminal = () => useContext<TerminalContext>(Context);
